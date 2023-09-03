f1482c7b563cac8b842ad7565c173ee098c27de9
William <goiswilliam194@gmail.com>
2023-09-03 15:24:07 -0300

    Initial Commit

---
+++ todo_list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo_list
+//
+//  Created by Icaro on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
